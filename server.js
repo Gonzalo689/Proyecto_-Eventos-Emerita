0abcb0113bd2db2e251a8e5deb6886c031d12753
Gonzalo Contreras <120806659+Gonzalo689@users.noreply.github.com>
2024-04-10 13:43:06 +0200

    .

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000;
+const eventsRouter = require('./routers/Events');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use('/events', eventsRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
