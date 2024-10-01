69b16b8b5957874d1c6be15c8d3e1d67e5563713
Gonzalo Contreras <120806659+Gonzalo689@users.noreply.github.com>
2024-10-01 10:27:30 +0200

    Delete Android_EventosEmerita directory

--- Android_EventosEmerita/app/src/main/java/com/example/android_eventosemerita/api/model/Category.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android_eventosemerita.api.model
-
-/**
- * Representa una categoría de Categorías.
- */
-class Category (val name:String ,val busqueda:String , val image: Int ) {
-}
--- Android_EventosEmerita/app/src/main/java/com/example/android_eventosemerita/api/model/Coment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android_eventosemerita.api.model
-
-/**
- * Representa una categoría de comentarios.
- */
-class Coment (val id:Int, val texto: String,val fecha:String, var listComents: List<Coment>, var idUser:Int, var idPost:Int){
-
-}
